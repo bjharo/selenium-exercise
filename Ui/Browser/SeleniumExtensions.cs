b46379a4ddb22def3629d1c3a355a47353840f4f
Bryan Haro <bryan.haro@bloomerang.com>
2023-09-30 22:19:16 -0400

    Add a done animating check

--- Ui/Browser/SeleniumExtensions.cs
+++ Ui/Browser/SeleniumExtensions.cs
@@ -7,33 +7,33 @@ public static class SeleniumExtensions
 {
     private static TimeSpan DefaultExplicitTimeout => TimeSpan.FromSeconds(60);
 
-    public static IWebElement GetElement(this ISearchContext context, By Selector, TimeSpan? timeout = null)
+    public static IWebElement GetElement(this ISearchContext context, By selector, TimeSpan? timeout = null)
     {
         var actualTimeout = timeout ?? DefaultExplicitTimeout;
 
         try
         {
-            return CreateWait(context, actualTimeout).Until(c => c.FindElement(Selector));
+            return CreateWait(context, actualTimeout).Until(c => c.FindElement(selector));
         }
         catch (Exception ex) when (ex is WebDriverTimeoutException or WebDriverException)
         {
             if (ex is WebDriverTimeoutException
                 || ex.Message.Contains($"timed out after {actualTimeout.TotalSeconds} seconds"))
             {
-                throw new NoSuchElementException($"Unable to find element with Selector {Selector.ToString()}.", ex);
+                throw new NoSuchElementException($"Unable to find element with Selector {selector.ToString()}.", ex);
             }
 
             throw;
         }
     }
 
-    public static bool WaitForElementToBeDisplayed(this ISearchContext driver, By Selector, TimeSpan? timeout = null)
+    public static bool WaitForElementToBeDisplayed(this ISearchContext driver, By selector, TimeSpan? timeout = null)
     {
         var actualTimeout = timeout ?? DefaultExplicitTimeout;
 
         try
         {
-            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(Selector).Displayed);
+            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(selector).Displayed);
         }
         catch (WebDriverTimeoutException)
         {
@@ -55,7 +55,7 @@ public static class SeleniumExtensions
         element.SendKeys(Keys.Backspace + newText);
     }
 
-    public static bool WaitForElementToNotBeDisplayed(this ISearchContext driver, By Selector, TimeSpan? timeout = null)
+    public static bool WaitForElementToNotBeDisplayed(this ISearchContext driver, By selector, TimeSpan? timeout = null)
     {
         var actualTimeout = timeout ?? DefaultExplicitTimeout;
 
@@ -65,7 +65,7 @@ public static class SeleniumExtensions
             {
                 try
                 {
-                    var isDisplayed = e.FindElement(Selector).Displayed;
+                    var isDisplayed = e.FindElement(selector).Displayed;
                     return !isDisplayed;
                 }
                 catch (Exception ex) when (ex is NoSuchElementException or StaleElementReferenceException)
@@ -80,13 +80,13 @@ public static class SeleniumExtensions
         }
     }
 
-    public static bool WaitForElementToHaveText(this ISearchContext driver, By Selector, string text, TimeSpan? timeout = null)
+    public static bool WaitForElementToHaveText(this ISearchContext driver, By selector, string text, TimeSpan? timeout = null)
     {
         var actualTimeout = timeout ?? DefaultExplicitTimeout;
 
         try
         {
-            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(Selector).Text.Equals(text));
+            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(selector).Text.Equals(text));
         }
         catch (WebDriverTimeoutException)
         {
@@ -94,13 +94,13 @@ public static class SeleniumExtensions
         }
     }
     
-    public static bool WaitForElementToContainText(this ISearchContext driver, By Selector, string text, TimeSpan? timeout = null)
+    public static bool WaitForElementToContainText(this ISearchContext driver, By selector, string text, TimeSpan? timeout = null)
     {
         var actualTimeout = timeout ?? DefaultExplicitTimeout;
 
         try
         {
-            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(Selector).Text.Contains(text));
+            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(selector).Text.Contains(text));
         }
         catch (WebDriverTimeoutException)
         {
@@ -108,13 +108,13 @@ public static class SeleniumExtensions
         }
     }
     
-    public static bool WaitForElementToHaveValue(this ISearchContext driver, By Selector, string text, TimeSpan? timeout = null)
+    public static bool WaitForElementToHaveValue(this ISearchContext driver, By selector, string text, TimeSpan? timeout = null)
     {
         var actualTimeout = timeout ?? DefaultExplicitTimeout;
 
         try
         {
-            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(Selector).GetAttribute("value").Equals(text));
+            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(selector).GetAttribute("value").Equals(text));
         }
         catch (WebDriverTimeoutException)
         {
@@ -122,18 +122,58 @@ public static class SeleniumExtensions
         }
     }
 
-    public static bool WaitForElementToBeEnabled(this ISearchContext driver, By Selector, TimeSpan? timeout = null)
+    public static bool WaitForElementToBeEnabled(this ISearchContext driver, By selector, TimeSpan? timeout = null)
     {
         var actualTimeout = timeout ?? DefaultExplicitTimeout;
 
         try
         {
-            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(Selector)).Enabled;
+            return CreateWait(driver, actualTimeout).Until(d => d.FindElement(selector)).Enabled;
         }
         catch (WebDriverTimeoutException)
         {
             return false;
         }
+    }
+    
+    public static bool WaitForElementToStopAnimating(this ISearchContext driver, By selector, Int32 timeoutInMs = 5000, Int32 waitTimeInMs = 250)
+    {
+        var doneMoving = false;
+
+        var prevCss = string.Empty;
+        var prevStyle = string.Empty;
+        var cumulative = 0;
+
+        for (var count = 0; count < 10; count++)
+        {
+            try
+            {
+                var css = driver.GetElement(selector).GetAttribute("class");
+                var style = driver.GetElement(selector).GetAttribute("style");
+
+                if (css.Equals(prevCss) || style.Equals(prevStyle))
+                {
+                    cumulative++;
+                    if (cumulative > 2)
+                    {
+                        doneMoving = true;
+                        break;
+                    }
+                }
+                else if (cumulative > 0)
+                {
+                    cumulative--;
+                }
+
+                prevCss = css;
+                prevStyle = style;
+            }
+            catch (StaleElementReferenceException) { }
+
+            Thread.Sleep(waitTimeInMs);
+        }
+
+        return doneMoving;
     }
 
     private static DefaultWait<ISearchContext> CreateWait(ISearchContext driver, TimeSpan timeout)
